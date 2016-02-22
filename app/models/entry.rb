1c1fa82e4b0e7aab21e997499b33b6d37ac8beee
Brittany Ko <bko@Brittanys-MacBook-Pro.local>
2016-02-21 19:57:53 -0800

    Added base models with relations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,10 +1,16 @@
 class Entry
   include Mongoid::Document
 
-  field :elo, type: Integer
+  field :elo, type: Integer, default: 0
 
   belongs_to :user
   belongs_to :league
   has_and_belongs_to_many :teams
+
+  validates :league, presence: true
+
+  def record_game
+  end
+
 
 end
