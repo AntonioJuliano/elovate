58ba27e692488942149682b45b9f756f061512d4
Antonio Juliano <ajuliano@princeton.edu>
2016-09-08 01:14:34 -0400

    upgrading to rails 5, changed models a bit

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,12 +2,13 @@ class Entry
   include Mongoid::Document
 
   field :elo, type: Integer, default: 0
+  field :manager, type: Boolean, default: false
 
   belongs_to :user
   belongs_to :league
   has_and_belongs_to_many :teams
 
-  validates :league, presence: true
+  validates_presence_of :league, :user, :elo
 
   def record_game
   end
--- spec/factories/entries.rb
+++ spec/factories/entries.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :entry do
     elo 5
-    league {create(:league)}
+    league { create(:league) }
+    user { create(:user) }
   end
 end
