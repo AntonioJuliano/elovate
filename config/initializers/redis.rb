c5dacbb50f67c5c3277fdc77ddc5924b5cd079e4
Antonio Juliano <antonio.m.juliano@gmail.com>
2016-09-08 17:56:00 -0700

    configure redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,8 @@
-$redis = Redis.new(:host => '0.0.0.0', :port => 6379)
+conf_file = File.join('config','redis.yml')
+
+$redis = if File.exists?(conf_file)
+  conf = YAML.load(File.read(conf_file))
+  conf[Rails.env.to_s].blank? ? Redis.new : Redis.new(conf[Rails.env.to_s])
+else
+  Redis.new
+end
